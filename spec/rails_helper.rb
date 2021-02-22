758d42550128137e6ed4bad010b74ce5f49c2ded
yamatsuta <jun_yamatsuta@yahoo.co.jp>
2021-02-22 14:14:24 +0900

    作成したloginhelperをrails_helperでinclude

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   #FactoryBotの記述を省略
   config.include FactoryBot::Syntax::Methods
+
+  config.include LoginHelpers
 end
